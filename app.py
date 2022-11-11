64a6c2b0c4067a51bf0a4ad5853904989c633adc
Veda Bojar <100831180+vedabojar@users.noreply.github.com>
2022-11-11 09:47:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ app.layout = html.Div(children=[
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True)
-    app.run_server(host = '127.0.0.1', port = '8050',debug=True)
+    app.run_server(debug=True)
+    #app.run_server(host = '127.0.0.1', port = '8050',debug=True)

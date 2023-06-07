f2b741a45d02a313465735aa959105b69fe3f1d2
Veda Bojar <vedabojar@gmail.com>
2023-06-07 17:10:33 +0200

    create logos 70%

--- functions/drawing_logos.py
+++ functions/drawing_logos.py
@@ -3,139 +3,161 @@
 # Import packages
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-import numpy as np
-import io
-import base64
-from PIL import Image
+import colorsys
 
 
-def fig2img(fig):
-    # Convert Matplotlib figure to image
-    buf = io.BytesIO()
-    fig.savefig(buf, format='png')
-    buf.seek(0)
-    img = Image.open(buf)
-    return img
 
 
-def image_to_base64(img):
-    # Convert image to base64 encoding
-    buf = io.BytesIO()
-    img.save(buf, format='png')
-    encoded_img = base64.b64encode(buf.getvalue()).decode()
-    return encoded_img
+
+def darken_color(color, percentage):
+    # Extract the RGB values and alpha channel from the color
+    r = color['r']
+    g = color['g']
+    b = color['b']
+    a = color['a']
+
+    # Calculate the darkening factor
+    factor = 1 - (percentage / 100)
+
+    # Darken each RGB channel by applying the darkening factor
+    new_r = max(0, int(r * factor))
+    new_g = max(0, int(g * factor))
+    new_b = max(0, int(b * factor))
+
+    # Return the updated color with the original alpha value
+    return {'r': new_r, 'g': new_g, 'b': new_b, 'a': a}
 
 
-def create_fig(start, end, domain_list = None):
+
+
+def create_fig(length, logo_color, domains_list = None):
     '''
     Creating a protein logo figure including the domain inside it.
     Args:
-        start: User input start of protein
-        end: User input end of protein
+        length: Length of the protein arrow
+        logo_color: Color of the protein arrow
         domain_list: contains any domains specified by user
     Returns:
         Returns a figure (graph) with protein and and its domains
     '''
+    if length == 0:
+        return {}
+    else:
+        arrow_start = 0
+        arrow_end = length
+        arrow_width = 6
+        arrow_point = arrow_width / 2
+        arrow_head = length * 0.1
+        arrow_minus_head = length - arrow_head
+        x_padding = 50
+        y_padding = 20
+        fill_color = logo_color['rgb']
+        outline_color = darken_color(fill_color, 30)  # Make outline darker
+        outline_width = 2  # Adjust the outline width as needed
 
-    # Set up the figure
-    fig, ax = plt.subplots(figsize=(10, 3))
+        shapes = [
+            {
+                'type': 'path',
+                'path': f'M0,0 L{arrow_minus_head},0 L{length},{arrow_point} L{arrow_minus_head},{arrow_width} L0,{arrow_width} Z',
+                'fillcolor': fill_color,
+                'line': {
+                    'width': arrow_width,
+                    'color': fill_color
+                },
+                'xref': 'x',
+                'yref': 'y',
+                'x0': arrow_start,
+                'y0': 0,
+                'x1': arrow_end,
+                'y1': arrow_width
+            },
+            {
+                'type': 'path',
+                'path': f'M0,0 L{arrow_minus_head},0 L{length},{arrow_point} L{arrow_minus_head},{arrow_width} L0,{arrow_width} Z',
+                'fillcolor': 'rgba({}, {}, {}, {})'.format(fill_color['r'], fill_color['g'], fill_color['b'], fill_color['a']),
+                'line': {
+                    'width': arrow_width - outline_width,
+                    'color': 'rgba({}, {}, {}, {})'.format(outline_color['r'], outline_color['g'], outline_color['b'], outline_color['a'])
+                },
+                'xref': 'x',
+                'yref': 'y',
+                'x0': arrow_start,
+                'y0': 0,
+                'x1': arrow_end,
+                'y1': arrow_width
+            }
+        ]
+        if domains_list != '':
+            for domain in domain_list:
+                domain_start = domain['start']
+                domain_end = domain['end']
+                domain_name = domain['name']
+                domain_middle = (domain_start + domain_end) / 2
 
-    # Define arrow parameters
-    arrow_start = start
-    arrow_size = end - start
-    arrow_height = 0.4
+                # Create a shape that matches the arrow shape
+                arrow_shape = f'M{domain_start},0 L{arrow_minus_head},0 L{length},{arrow_point} L{arrow_minus_head},{arrow_width} L{domain_start},{arrow_width} Z'
 
-    # Create the protein arrow
-    ax.arrow(
-        arrow_start, # protein start
-        0, # y_level
-        arrow_size, # protein size
-        0, # y_level upper
-        head_width=0.2, 
-        head_length=0.2, 
-        fc="gray", 
-        ec="black"
-    )
+                # Adjust the domain shape if it doesn't cover the full arrow head
+                if domain_end < arrow_minus_head:
+                    domain_shape = f'M{domain_start},0 L{domain_end},0 L{domain_end},{arrow_width} L{domain_start},{arrow_width} Z'
+                else:
+                    domain_shape = arrow_shape
 
-    # Create the domain arrows
-    if domain_list:
-        for domain in domain_list:
-            domain_start = domain[0] - start
-            domain_end = domain[1] - start
+                shapes.append(
+                    {
+                        'type': 'path',
+                        'path': domain_shape,
+                        'fillcolor': 'rgba(0, 0, 0, 0.2)',  # Customize the fill color for the highlighted area
+                        'line': {
+                            'width': 0
+                        },
+                        'xref': 'x',
+                        'yref': 'y',
+                        'x0': arrow_start,
+                        'y0': 0,
+                        'x1': arrow_end,
+                        'y1': arrow_width
+                    }
+                )
 
-            # Draw the domain arrow
-            ax.arrow(
-                domain_start,
-                0,
-                domain_end - domain_start,
-                arrow_height,
-                head_width=0.2,
-                head_length=0.2,
-                fc="red",
-                ec="black",
-            )
+                shapes.append(
+                    {
+                        'type': 'text',
+                        'xref': 'x',
+                        'yref': 'y',
+                        'x': domain_middle,
+                        'y': arrow_width + 10,  # Adjust the vertical position as needed
+                        'text': domain_name,
+                        'showarrow': False,
+                        'font': {
+                            'size': 12,
+                            'color': 'black'
+                        }
+                    }
+                )
+    return {
+        'data': [],
+        'layout': {
+            'shapes': shapes,
+            'xaxis': {
+                'range': [arrow_start - x_padding, arrow_end + x_padding],
+                'constrain': 'domain',
+                'showticklabels': False
+            },
+            'yaxis': {
+                'range': [-y_padding, arrow_width + y_padding],
+                'constrain': 'domain',
+                'showticklabels': False
+            },
+            'height': 400,
+            'width': 800,
+            'margin': {'t': 0, 'b': 0, 'l': 0, 'r': 0},
+            'showlegend': False,
+            'plot_bgcolor': 'white',
+            'paper_bgcolor': 'white'
+        }
+    }
 
-            # Annotate the domain name
-            text_x = domain_start + (domain_end - domain_start) / 2
-            ax.annotate(
-                domain[2],
-                xy=(text_x, arrow_height),
-                xytext=(0, 5),
-                textcoords="offset points",
-                ha="center",
-                fontsize=8,
-                color="black",
-            )
-
-    # Set the x-axis limits
-    ax.set_xlim(arrow_start - 1, arrow_size + 1)
-
-    # Remove y-axis ticks and labels
-    ax.yaxis.set_ticks([])
-
-    # Set the figure background color
-    fig.patch.set_facecolor("white")
-
-    # Remove plot frame
-    ax.spines["top"].set_visible(False)
-    ax.spines["bottom"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.spines["left"].set_visible(False)
-
-    # Set plot size and margins
-    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.1)
-
-    # Convert the Matplotlib figure to Plotly Figure object
-    fig_plotly = go.Figure()
-
-    # Convert the Matplotlib figure to an image
-    img = fig2img(fig)
-    encoded_img = image_to_base64(img)
-
-    # Add the image to Plotly Figure
-    fig_plotly.add_layout_image(
-        dict(
-            source=f"data:image/png;base64,{encoded_img}",
-            xref="x",
-            yref="y",
-            x=arrow_start,
-            y=0,
-            sizex=arrow_size,
-            sizey=arrow_height,
-            sizing="stretch",
-            layer="below",
-        )
-    )
-
-    # Set the layout of Plotly Figure
-    fig_plotly.update_layout(
-        xaxis={"visible": False},
-        yaxis={"visible": False},
-        width=800,
-        height=800,
-    )
-
-    return fig_plotly
 
 
 
